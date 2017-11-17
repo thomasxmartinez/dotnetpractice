c1abfb245f6e57fe36baf2456467d864e8f37926
tommytnez <tommytnez@yahoo.com>
2017-11-16 19:13:13 -0800

    csharp

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace powerfivebackend {
+    class Program {
+        static void Main (string[] args) {
+            var names = new List<string> { "Thomas", "Ana", "Felipe" };
+            names.Add ("Maria");
+            names.Add ("Bill");
+            names.Remove ("Ana");
+            foreach (var name in names) {
+                Console.WriteLine ($"Hello {name.ToUpper()}!");
+            }
+            Console.WriteLine ($"My name is {names[0]}");
+            Console.WriteLine ($"I've added {names[2]} and {names[3]} to the list");
+        }
+    }
+}
